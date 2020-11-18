fdaa9a51bf77a796df9c30ebae016ddc495f5141
nicoletaroman <70715983+nicoletaroman@users.noreply.github.com>
2020-11-18 13:37:36 +0200

    Add files via upload

---
+++ pr 3.py
@@ -0,0 +1,10 @@
+c1=str(input("dati un cuvant"))
+c2=str(input("dati un cuvant"))
+c3=str(input("dati un cuvant"))
+c4=str(input("dati un cuvant"))
+print("cuvantul 1 este",c1)
+print("cuvantul 2 este",c2)
+print("cuvantul 3 este",c3)
+print("cuvantul 4 este",c4)
+s=c1 + " " + c2 + " "+c3  + " " + c4 + "."
+print(s)
